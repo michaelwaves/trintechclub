e9b242f528bd6d178ac17950632b7230fac2d7ce
michaelwaves <michaelyu713705@gmail.com>
2023-09-29 12:15:02 +0800

    underlined learn more

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -23,7 +23,7 @@ export default function Hero() {
                     <PrintButton />
                     <DiscordButton />
                 </div>
-                <Link href="/about">Learn More</Link>
+                <Link href="/about" className="underline">Learn More</Link>
             </div>
         </div>
     )
